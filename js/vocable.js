a66e4a55abb9c514e7d4cb1422ce91b976cf17c1
svnderhvrtog <sander-hartog@outlook.com>
2024-12-27 17:53:48 +0100

    small update on vocable

--- js/vocable.js
+++ js/vocable.js
@@ -7,6 +7,7 @@ const reverseBtn = document.getElementById('reverse-btn');
 const capitalBtn = document.getElementById('capital-btn');
 const emojifyBtn = document.getElementById('emojify-btn');
 const palindromeBtn = document.getElementById('palindrome-btn');
+const noDuplicatesBtn = document.getElementById('no-duplicates-btn');
 
 panicBtn.addEventListener('mouseover', () => panicBtn.src = '../images/vocable/text-frame-hover1.png');
 panicBtn.addEventListener('mouseout', () => panicBtn.src = '../images/vocable/text-frame1.png');
@@ -45,18 +46,31 @@ reverseBtn.addEventListener('mouseover', () => reverseBtn.src = '../images/vocab
 reverseBtn.addEventListener('mouseout', () => reverseBtn.src = '../images/vocable/text-frame4.png');
 reverseBtn.addEventListener('click', () => { reverseFunc(inputField.value) });
 function reverseFunc(text) {
-    let newText = ''
+    let newText = '';
     for(let i = text.length - 1; i >= 0; i--){
         newText += text[i]
-    }
+    };
     return inputField.value = newText
 }
 
 capitalBtn.addEventListener('mouseover', () => capitalBtn.src = '../images/vocable/text-frame-hover5.png');
 capitalBtn.addEventListener('mouseout', () => capitalBtn.src = '../images/vocable/text-frame5.png');
+capitalBtn.addEventListener('click', () => { capitalFunc(inputField.value) });
+function capitalFunc(text) {
+    let newText = text.split(' ');
+    let completeText = newText.map(word => capitalizeWord(word));
+    return inputField.value = completeText.join(' ')
+};
+
+function capitalizeWord(word){
+    return word[0].toUpperCase() + word.slice(1)
+};
 
 emojifyBtn.addEventListener('mouseover', () => emojifyBtn.src = '../images/vocable/text-frame-hover6.png');
 emojifyBtn.addEventListener('mouseout', () => emojifyBtn.src = '../images/vocable/text-frame6.png');
 
 palindromeBtn.addEventListener('mouseover', () => palindromeBtn.src = '../images/vocable/text-frame-hover7.png');
 palindromeBtn.addEventListener('mouseout', () => palindromeBtn.src = '../images/vocable/text-frame7.png');
+
+noDuplicatesBtn.addEventListener('mouseover', () => noDuplicatesBtn.src = '../images/vocable/text-frame-hover8.png');
+noDuplicatesBtn.addEventListener('mouseout', () => noDuplicatesBtn.src = '../images/vocable/text-frame8.png');
